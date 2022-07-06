2e8d46d78573414a8536afd3f1f75b1593463ca0
Devin Fischer <devinfischer32@gmail.com>
2022-07-06 15:25:11 -0700

    Restructure and installed packages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
       <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center ">
-        <div className="w-16 h-16 bg-red-200 "></div>
+        <div className="w-16 h-16 bg-red-200"></div>
         <div className="p-8">Vs</div>
         <div className="w-16 h-16 bg-red-200"></div>
       </div>
