6478d193cabd703e7b1745a83aadd50902b5e38f
Devin Fischer <devinfischer32@gmail.com>
2022-07-12 18:17:18 -0700

    Add github link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,6 +48,9 @@ const Home = () => {
 
         <div className="p-2" />
       </div>
+      <div className="absolute bottom-0 w-full text-xl text-center pb-2">
+        <a href="https://github.com/DevinFischer32/poki_tut">Github</a>
+      </div>
     </div>
   );
 };
